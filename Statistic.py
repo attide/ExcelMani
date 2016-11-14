5b39f13883c5d42de52915a9296e6b1aff51c9a0
yintianqi <yintianqi@baidu.com>
2016-11-14 17:53:16 +0800

    Copy python2 files

---
+++ Statistic.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u'统计Priority为0和1，统计创建时间和解决时间相差超过8小时的'
+
+u'Priority为2和3，统计创建时间和解决时间相差超过24小时的'
+
+u'输入表格：编号、标题、负责人、创建时间、解决时间、时间差（单位小时）'
+
+__author__ = 'Fengyue Zhu'
+
+import xlrd
+import xlwt
+import sys
+import re
+from datetime import date,datetime,time
+from xlrd import open_workbook,xldate_as_tuple
+
+PRIORITY = u'Priority'
+START_TIME = u'创建时间'
+SOLVED_TIME = u'解决时间'
+BUG_ID = u'编号'
+TITLE = u'标题'
+OWNER = u'负责人'
+TIME_DIFF = u'时间差（单位小时）'
+RESOLUTION = u'Resolution'
+NOT_A_BUG = u'Not a Bug'
+P0_PRIORITY = u'P0-Highest'
+P1_PRIORITY = u'P1-High'
+P2_PRIORITY = u'P2-Middle'
+P3_PRIORITY = u'P3-Lowest'
+
+OUTPUT_XLS = 'sta_res.xls'
+
+def getIdx(filename,key):
+    bk = xlrd.open_workbook(filename)
+    try:
+        sh = bk.sheet_by_index(0)
+    except:
+        print 'no sheet in %s' % filename
+        return
+    first_row = sh.row_values(0)
+    if key in first_row:
+        target_index = first_row.index(key)
+    else:
+        print '\'', key ,'\' not in subtitle'
+        return
+    return target_index
+
+def getContent(filename):
+    bk = xlrd.open_workbook(filename)
+    try:
+        sh = bk.sheet_by_index(0)
+    except:
+        print 'no sheet in %s' % filename
+        return
+    nrows = sh.nrows
+    row_list = []
+    for i in range(1,nrows):
+        row_data = sh.row_values(i)
+        row_list.append(row_data)
+    return row_list
+
+def getDatetimeVal(datetime_num,filename):
+    bk = xlrd.open_workbook(filename)
+    try:
+        sh = bk.sheet_by_index(0)
+    except:
+        print 'no sheet in %s' % filename
+        return
+    datetime_value = xldate_as_tuple(datetime_num,bk.datemode)
+    return datetime(*datetime_value)
+
+def resXlwt(result_list_p01,result_list_p23):
+    book = xlwt.Workbook()
+    if(result_list_p01 is None):
+        pass
+    else:
+        sheet1 = book.add_sheet('Result P0,P1',cell_overwrite_ok=True)
+        row_num = 0
+        cell_num = 0
+        for row in result_list_p01:
+            for cell in row:
+                sheet1.write(row_num,cell_num,cell)
+                cell_num+=1
+            row_num+=1
+            cell_num=0
+    if(result_list_p23 is None):
+        pass
+    else:
+        sheet2 = book.add_sheet('Result P2,P3',cell_overwrite_ok=True)
+        row_num = 0
+        cell_num = 0
+        for row in result_list_p23:
+            for cell in row:
+                sheet2.write(row_num,cell_num,cell)
+                cell_num+=1
+            row_num+=1
+            cell_num=0
+    book.save(OUTPUT_XLS)
+    print 'Sheet Result has bee added'
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 2:
+        file_name = args[1].decode('utf-8')
+        priority_idx = getIdx(file_name, PRIORITY)
+        start_time_idx = getIdx(file_name, START_TIME)
+        solved_time_idx = getIdx(file_name, SOLVED_TIME)
+        bug_id_idx = getIdx(file_name, BUG_ID)
+        title_idx = getIdx(file_name,TITLE)
+        owner_idx = getIdx(file_name,OWNER)
+        resolution_idx = getIdx(file_name,RESOLUTION)
+        content = getContent(file_name)
+        # 删除'Not a bug'的行
+        bug_list = []
+        for row_i in content:
+            if row_i[resolution_idx] == NOT_A_BUG:
+                pass
+            else:
+                bug_list.append(row_i)
+        # print len(bug_list)
+        subtitle = [BUG_ID,TITLE,OWNER,PRIORITY,START_TIME,SOLVED_TIME,TIME_DIFF]
+        result_list_p01 = []
+        result_list_p01.append(subtitle)
+        result_list_p23 = []
+        result_list_p23.append(subtitle)
+        for row_i in bug_list:
+            start_time_val = getDatetimeVal(row_i[start_time_idx],file_name)
+            end_time_val = getDatetimeVal(row_i[solved_time_idx],file_name)
+            time_diff_val = (end_time_val-start_time_val).total_seconds()/(60*60)
+            if (row_i[priority_idx] in (P2_PRIORITY,P3_PRIORITY) and time_diff_val >= 24):
+            # if True:
+                res_row = [row_i[bug_id_idx],row_i[title_idx],row_i[owner_idx],row_i[priority_idx],
+                    str(start_time_val),
+                    str(end_time_val),
+                    int(time_diff_val)]
+                result_list_p23.append(res_row)
+            if (row_i[priority_idx] in (P0_PRIORITY,P1_PRIORITY) and time_diff_val >= 8):
+                res_row = [row_i[bug_id_idx],row_i[title_idx],row_i[owner_idx],row_i[priority_idx],
+                    str(start_time_val),
+                    str(end_time_val),
+                    int(time_diff_val)]
+                result_list_p01.append(res_row)
+        resXlwt(result_list_p01,result_list_p23)
+
+    else:
+        print 'Invalid arguments'
---
+++ v4_ExcelMani.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u'生成线下质量分析统计的相关表格'
+
+__author__ = 'Fengyue Zhu'
+
+import xlrd
+import xlwt
+import sys
+import re
+from datetime import date,datetime,time
+from xlrd import open_workbook,xldate_as_tuple
+
+STAGE = u'可前置在哪个阶段发现'
+SEVERITY = u'Severity'
+# PRIORITY = u'Priority'
+RESOLUTION = u'Resolution'
+OWNER = u'负责人'
+START_TIME = u'创建时间'
+SOLVED_TIME = u'解决时间'
+TITLE = u'标题'
+
+DISTRIBUTION = u'每日分布'
+MODULE_DIS = u'模块分布'
+
+NOT_A_BUG = u'Not a Bug'
+
+OUTPUT_XLS = 'v4_res.xls'
+
+def getIdx(first_row,key):
+    if key in first_row:
+        target_index = first_row.index(key)
+    else:
+        print '\'', key ,'\' not in subtitle'
+        return
+    return target_index
+
+def getFirstName(name_data):
+    name_list = re.split(r'\,+', name_data)
+    first_name = re.split(r'\(', name_list[0])[0]
+    return first_name
+
+def getModule(title_data):
+    if re.match(u'【(.*?)】', title_data):
+        module_key = re.match(u'【(.*?)】', title_data).groups()[0]
+        if u"直播" in module_key:
+            module_key = u'直播'
+        if u"组件" in module_key:
+            module_key = u'组件'
+    else:
+        module_key = title_data
+    if any(crash_key in module_key for crash_key in (u'崩溃',u'Crash',u'crash',u'闪退')):
+        module_key = u'崩溃'
+    return module_key
+
+def getDateStr(date_val,bk):
+    date_value = xldate_as_tuple(date_val,bk.datemode)
+    date_key = date(*date_value[:3])
+    return str(date_key)
+
+def countCol(target_col):
+    result = {}
+    for vals in target_col:
+        if vals not in result:
+            result[vals] = 0
+        result[vals] += 1
+    return result
+
+def mergeDate(start_list,solved_list):
+    result = {}
+    for vals in start_list:
+        if vals not in result:
+            result[vals] = [0,0]
+        result[vals][0] += 1
+    for vals in solved_list:
+        if vals not in result:
+            result[vals] = [0,0]
+        result[vals][1] += 1
+    return result
+
+def comXlwt(book, dic, key):
+    if(dic is None):
+        return
+    sheet1 = book.add_sheet(key,cell_overwrite_ok=True)
+    sheet1.write(0,0,key)
+    sheet1.write(0,1,u'数量')
+    row_num = 1
+    val_sum = 0
+    for i in dic:
+        if i == '':
+            sheet1.write(row_num,0,u'未知')
+        else:
+            sheet1.write(row_num,0,i)
+        sheet1.write(row_num,1,dic[i])
+        row_num+=1
+        val_sum+=dic[i]
+    sheet1.write(row_num,0,u'合计')
+    sheet1.write(row_num,1,val_sum)
+    print 'Sheet \'', key,' \' has bee added'
+
+def dateXlwt(book, dic, key):
+    if(dic is None):
+        return
+    sheet1 = book.add_sheet(key,cell_overwrite_ok=True)
+    sheet1.write(0,0,key)
+    sheet1.write(0,1,u'数量')
+    row_num = 1
+    val_sum1 = 0
+    val_sum2 = 0
+    for i in dic:
+        if i == '':
+            sheet1.write(row_num,0,u'未知')
+        else:
+            sheet1.write(row_num,0,i)
+        sheet1.write(row_num,1,dic[i][0])
+        sheet1.write(row_num,2,dic[i][1])
+        row_num+=1
+        val_sum1+=dic[i][0]
+        val_sum2+=dic[i][1]
+    sheet1.write(row_num,0,u'合计')
+    sheet1.write(row_num,1,val_sum1)
+    sheet1.write(row_num,2,val_sum2)
+    print 'Sheet \'', key,' \' has bee added'
+
+def main(file_name):
+    bk = xlrd.open_workbook(file_name)
+    try:
+        sh = bk.sheet_by_index(0)
+    except:
+        print 'no sheet in %s' % filename
+        return
+    first_row = sh.row_values(0)
+    nrows = sh.nrows
+    stage_idx = getIdx(first_row,STAGE)
+    severity_idx = getIdx(first_row,SEVERITY)
+    resolution_idx = getIdx(first_row,RESOLUTION)
+    owner_idx = getIdx(first_row,OWNER)
+    start_time_idx = getIdx(first_row, START_TIME)
+    solved_time_idx = getIdx(first_row, SOLVED_TIME)
+    title_idx = getIdx(first_row,TITLE)
+    stage_col = []
+    severity_col = []
+    resolution_col = []
+    owner_col = []
+    start_time_col = []
+    solved_time_col = []
+    module_col = []
+    for i in range(1,nrows):
+        row_data = sh.row_values(i)
+        if row_data[resolution_idx] == NOT_A_BUG:
+            continue
+        else:
+            stage_col.append(row_data[stage_idx])
+            severity_col.append(row_data[severity_idx])
+            resolution_col.append(row_data[resolution_idx])
+            owner_col.append(getFirstName(row_data[owner_idx]))
+            module_col.append(getModule(row_data[title_idx]))
+            start_time_col.append(getDateStr(row_data[start_time_idx],bk))
+            solved_time_col.append(getDateStr(row_data[solved_time_idx],bk))
+    book = xlwt.Workbook()
+    comXlwt(book,countCol(stage_col),STAGE)
+    comXlwt(book,countCol(severity_col),SEVERITY)
+    comXlwt(book,countCol(resolution_col),RESOLUTION)
+    comXlwt(book,countCol(owner_col),OWNER)
+    comXlwt(book,countCol(module_col),MODULE_DIS)
+    dateXlwt(book,mergeDate(start_time_col,solved_time_col),DISTRIBUTION)
+    book.save(OUTPUT_XLS)
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 2:
+        # print args[1]
+        file_name = args[1].decode('utf-8')
+        main(file_name)
+    else:
+        print 'Invalid arguments'
+
