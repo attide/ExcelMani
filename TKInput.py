bb16ca539d3fa982f04a521c132ef21ad3842ef8
yintianqi <yintianqi@baidu.com>
2016-11-16 18:42:45 +0800

    Modify tk to excelmani

--- TKInput.py
+++ TKInput.py
@@ -3,7 +3,7 @@
 
 from Tkinter import *
 import tkMessageBox
-import v3_ExcelMani
+import ExcelMani
 
 class Application(Frame):
     def __init__(self, master=None):
@@ -14,14 +14,14 @@ class Application(Frame):
     def createWidgets(self):
         self.nameInput = Entry(self)
         self.nameInput.pack()
-        self.alertButton = Button(self, text='Get Res XLS', command=self.hello)
+        self.alertButton = Button(self, text='Get Res XLS', command=self.generateXls)
         self.alertButton.pack()
         self.quitButton = Button(self, text='Quit', command=self.quit)
         self.quitButton.pack()
 
-    def hello(self):
+    def generateXls(self):
         name = self.nameInput.get()
-        v3_ExcelMani.main(name)
+        ExcelMani.main(name)
 
 def main():
     app = Application()
